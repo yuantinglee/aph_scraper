f0579aef41831513d33fc1f12fdc4c7529683ae7
yuantinglee <yuantinglee96@gmail.com>
2019-12-14 23:20:03 +0100

    add distinction between speaker and deputy speaker

--- find_person_in_db_aph.py
+++ find_person_in_db_aph.py
@@ -6,10 +6,8 @@ from normality import normalize
 import parliament.models as pm
 import cities.models as cmodels
 
-def find_person_in_db_aph(name, add_info={}, create=True,
-                      first_entry_for_unresolved_ambiguity=True, verbosity=1):
+def find_person_in_db_aph(name, add_info={}, create=True, verbosity=1):
 
-    #if name.strip('-– ()') is '' or name is None:
     if name is None:
         print("! Warning: no valid name string given")
         if create:
@@ -51,10 +49,6 @@ def find_person_in_db_aph(name, add_info={}, create=True,
     #! could be important if speaker holds ministerial position
     # position = PERSON_POSITION.search(name)
     if "role" in add_info.keys():
-        # if position:
-        #    if add_info["position"] != position:
-        #        print("! Warning: position does not match ({}, {})".format(
-        #            position, add_info["position"]))
         position = add_info["role"]
     else:
         position = None
@@ -62,61 +56,72 @@ def find_person_in_db_aph(name, add_info={}, create=True,
         if position and verbosity > 1:
             print("= position: {}".format(position.group(0)))
 
-    #if "party" in add_info.keys() and not 'N/A':
-    #    party = add_info['party']
-    #    p_party, created = pm.Party.objects.get_or_create(name=party)
-    #else:
-    #    party = None
-
-    #if "electorate" in add_info.keys():
-    #    electorate = add_info['electorate']
-    #else:
-    #    electorate = None
-
     # find matching entry in database
     # provision for Speaker of Parliament
-    if name_id != '10000':
+    if name_id != '10000' or name_id != '110000':
         query = pm.Person.objects.filter(aph_id=name_id)
 
-    elif name_id == '10000':
-        postquery = pm.Post.objects.get(
-        parlperiod__n=pp,
-        start_date__lte = date,
-        end_date__gte = date
-        )
-
-        parl_speaker = postquery.person
-        name_id = parl_speaker.aph_id
-        return parl_speaker
-
-    if len(query) == 1:
-        return query.first()
-
-    elif len(query) > 1:
-        if party:
-            rquery = query.filter(party__alt_names__contains=[party])
-            if len(rquery) == 1:
-                return emit_person(rquery.first(), period=pp, party=party)
-            elif len(rquery) > 1:
-                query = rquery
-
-        if pp:
-            rquery = query.filter(in_parlperiod__contains=[pp])
-            if len(rquery) == 1:
-                return emit_person(rquery.first(), period=pp, party=party)
-            elif len(rquery) > 1:
-                query = rquery
-
-        print("! Warning: Could not distinguish between persons!")
-        print("For name string: {}".format(name))
-        print("first name: {}, surname: {}".format(firstname, surname))
-        print("Query: {}".format(query))
-
-        if first_entry_for_unresolved_ambiguity:
-            print('Taking first entry of ambiguous results')
+        if len(query) == 1:
             return query.first()
+
+    elif name_id == '10000' or name_id == '110000':
+        # check for name to see if speaker or deputy
+        if surname == "SPEAKER":
+            postquery = pm.Post.objects.get(
+            title='Speaker',
+            parlperiod__n=pp,
+            start_date__lte = date,
+            end_date__gte = date
+            )
+
+            parl_speaker = postquery.person
+            return parl_speaker
+
         else:
-            return None
+            rquery = pm.Post.objects.filter(
+            Q(title='Deputy Speaker') | Q(title='Second Deputy Speaker'),
+            parlperiod__n=pp,
+            start_date__lte = date,
+            end_date__gte = date
+            )
+
+            if len(rquery) == 1:
+                parl_speaker = rquery.first().person
+                return parl_speaker
+
+            elif len(rquery) > 1:
+                squery = rquery.filter(person__surname = surname)
+                parl_speaker = squery.first().person
+                return parl_speaker
+
+
+    # should not have a need for multiple query results any more?
+    #elif len(query) > 1:
+        #if party:
+        #    rquery = query.filter(party__alt_names__contains=[party])
+        #    if len(rquery) == 1:
+        #        return emit_person(rquery.first(), period=pp, party=party)
+        #    elif len(rquery) > 1:
+        #        query = rquery
+
+        #if pp:
+        #    rquery = query.filter(in_parlperiod__contains=[pp])
+        #    if len(rquery) == 1:
+                #return emit_person(rquery.first(), period=pp, party=party)
+        #        return emit_person(rquery.first(), period=pp)
+        #    elif len(rquery) > 1:
+        #        query = rquery
+
+        #print("! Warning: Could not distinguish between persons!")
+        #print("For name string: {}".format(name))
+        #print("first name: {}, surname: {}".format(firstname, surname))
+        #print("Query: {}".format(query))
+
+        #if first_entry_for_unresolved_ambiguity:
+        #    print('Taking first entry of ambiguous results')
+        #    return query.first()
+        #else:
+        #    return None
 
     # if query returns no results
     else:
@@ -125,20 +130,20 @@ def find_person_in_db_aph(name, add_info={}, create=True,
         if verbosity > 0:
             print("name: {}".format(name))
             print("first name: {}, surname: {}".format(firstname, surname))
+            print("name id: {}".format(name_id))
 
         if create:
             person = pm.Person(surname=surname, first_name=firstname)
 
-            if party:
-                try:
-                    party_obj = pm.Party.objects.get(name=party)
-                    person.party = party_obj
+            #if party:
+            #    try:
+            #        party_obj = pm.Party.objects.get(name=party)
+            #        person.party = party_obj
+            #    except pm.Party.DoesNotExist:
+            #        print("! Warning: party could not be identified: {}".format(party))
 
-                except pm.Party.DoesNotExist:
-                    print("! Warning: party could not be identified: {}".format(party))
-
-            if position:
-                person.positions = [position]
+            #if position:
+            #    person.positions = [position]
                 # use position with data model "Post" ?
 
             if name_id != 10000 and not None:
@@ -159,20 +164,19 @@ def find_person_in_db_aph(name, add_info={}, create=True,
 
             person.save()
 
-            seat, created = pm.Seat.objects.get_or_create(
-            parlperiod=pm.ParlPeriod.objects.get(n=pp),
-            occupant=person
-            )
+            #seat, created = pm.Seat.objects.get_or_create(
+            #parlperiod=pm.ParlPeriod.objects.get(n=pp),
+            #occupant=person
+            #)
 
-            if electorate:
-                constituency, created = pm.Constituency.objects.get_or_create(
-                parliament=pm.Parl.objects.get(country=cmodels.Country.objects.get(name="Australia")),
-                name=electorate
-                )
-                constituency.save()
-                seat.constituency = constituency
+            #if electorate:
+            #    constituency, created = pm.Constituency.objects.get_or_create(
+            #    parliament = pm.Parl.objects.get(country=cmodels.Country.objects.get(name="Australia")),
+            #    name=electorate)
+            #    constituency.save()
+            #    seat.constituency = constituency
 
-            seat.save()
+            #seat.save()
 
             print("Created person: {}".format(person))
             return person
